86f33f31491b227122740e9457a1efee94c750a3
Luce <boama561@gmail.com>
2025-07-25 12:43:02 +0000

    Gestion du Timer dans le Header

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
 const { Server } = require('socket.io');
+const sessionRoutes = require('./routes/sessionRoutes'); // ✅ version MVC
+const { createSession } = require('../server/controllers/sessionControllers');
 
 dotenv.config();
 
@@ -22,7 +24,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/sessions', require('./routes/sessionRoutes'));
+app.use('/api', sessionRoutes);
 
 app.get('/api/test-db', async (req, res) => {
   try {
