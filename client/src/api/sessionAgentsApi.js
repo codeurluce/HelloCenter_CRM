3a3f277aa6daee525e3d654833875160234c6ab7
Luce <boama561@gmail.com>
2025-09-18 16:06:44 +0000

    Remplacements des fetch en axiosInstance

--- client/src/api/sessionAgentsApi.js
+++ client/src/api/sessionAgentsApi.js
@@ -1,24 +1,17 @@
+import axiosInstance from "./axiosInstance";
+
 export const updateSession = async (email, newStatus) => {
   try {
-    const response = await fetch('http://localhost:5000/api/sessions/update', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
+    const response = await axiosInstance.post('/sessions/update', {
         email,
         status: newStatus,
-      }),
     });
 
-    const data = await response.json();
-
-    if (!response.ok) throw new Error(data.message);
-
-    console.log('✅ Statut mis à jour avec succès :', data.message);
+    console.log('✅ Statut mis à jour avec succès :',  response.data.message);
     return true;
   } catch (err) {
-    console.error('❌ Erreur lors de la mise à jour de session :', err.message);
+   const errorMessage = err.response?.data?.message || err.message;
+    console.error('❌ Erreur lors de la mise à jour de session :', errorMessage);
     return false;
   }
 };
