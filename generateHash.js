8abe6a38e0096f7493d053c5ef1065e7eb2fcab8
Luce <boama561@gmail.com>
2025-09-18 13:01:25 +0000

    ajout de la variable d'env frontend

--- generateHash.js
+++
@@ -1,8 +0,0 @@
-// generateHash.js
-const bcrypt = require('bcrypt');
-
-const plainPassword = 'Test@123'; // <-- Remplace par ton mot de passe réel
-bcrypt.hash(plainPassword, 10, (err, hash) => {
-  if (err) throw err;
-  console.log('Mot de passe hashé :', hash);
-});
