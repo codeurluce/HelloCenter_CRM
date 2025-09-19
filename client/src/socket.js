91308a0b75b27e46c0b98a792247dcb9b81a4188
Luce <boama561@gmail.com>
2025-09-19 17:57:35 +0000

    changement url localhost dans socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,8 @@
 import { io } from 'socket.io-client';
 
 // 🔗 adapte à ton backend (si tu déploies en prod mets ton vrai domaine)
-const socket = io("http://localhost:5000", {
+const socket = io("http://crmhellocenterbackend-production.up.railway.app", {
+  // const socket = io("http://localhost:5000", {
   withCredentials: true,
   transports: ["websocket"],
 });
