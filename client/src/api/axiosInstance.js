096e2a8bb1da4f87483c453473d89edf8c557d2b
Luce <boama561@gmail.com>
2025-09-19 22:34:29 +0000

    modifications client

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://crmhellocenterbackend-production.up.railway.app/api', // Change cette URL pour la prod
+  baseURL: 'https://crmhellocenterbackend-production.up.railway.app/api', // Change cette URL pour la prod
 // baseURL: 'http://localhost:5000/api', 
   headers: {
     'Content-Type': 'application/json',
