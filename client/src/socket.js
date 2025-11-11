42484812cec86c9489b42f506e3c921ff8253ac3
Luce <boama561@gmail.com>
2025-11-11 12:00:56 +0000

    automatisation deploy frontend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,6 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-// 🔗 adapte à ton backend (si tu déploies en prod mets ton vrai domaine)
-// const socketUrl = BACKEND_URL || 'http://localhost:8080';
-
 const socket = io('/', {
   withCredentials: true,
   transports: ["websocket"],
